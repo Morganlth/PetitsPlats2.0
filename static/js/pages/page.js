3be2c55a2c70757c56dfce4aeacfb4c7c7c4b631
Morgan <lethuaut.morgan@gmail.com>
2024-07-28 15:25:21 +0200

    added "message"

--- static/js/pages/page.js
+++ static/js/pages/page.js
@@ -4,10 +4,13 @@
 // #\_IMPORTS_\
 
     // __JS
-    import { searchbar_init               } from './searchbar.js'
-    import { filters_init                 } from './filters.js'
-    import { tags_init                    } from './tags.js'
-    import { recipes_init, recipes_update } from './recipes.js'
+    import EVENTS from '../contexts/Events.js'
+
+    import { searchbar_init                          } from './searchbar.js'
+    import { filters_init                            } from './filters.js'
+    import { tags_init                               } from './tags.js'
+    import { message_init, message_updateTextContent } from './message.js'
+    import { recipes_init, recipes_update            } from './recipes.js'
 
 
 // #\_FUNCTIONS_\
@@ -18,9 +21,19 @@
 
     function body_set() { body_setEvents() }
 
-    function body_setEvents() { document.body?.addEventListener('research', body_eResearch) }
+    function body_setEvents()
+    {
+        EVENTS.events_add(
+        {
+            'writing' : body_eWriting,
+            'research': body_eResearch
+        },
+        document.body)
+    }
 
     // __EVENTS
+    function body_eWriting({detail}) { message_updateTextContent(detail.value) }
+
     function body_eResearch({detail}) { recipes_update(detail.value) }
 
 
@@ -34,4 +47,5 @@
         filters_init() // init filters before recipes !important
         tags_init()
         recipes_init()
+        message_init()
     }
